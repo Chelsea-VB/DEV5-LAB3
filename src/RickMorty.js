676c5eec2871ab9591b47831a396ed76e38327bd
Chelsea-VB <vanbreda.chelsea@skynet.be>
2022-10-24 16:59:28 +0200

    fix: change api key to temprature ; feat: check temperature and show character

--- src/RickMorty.js
+++ src/RickMorty.js
@@ -1,6 +1,6 @@
 export default class RickMorty {
-    constructor(api_key) {
-        this.apiKey = api_key;
+    constructor(temperature) {
+        this.temperature = temperature;
         this.getCharacters();
     };
 
@@ -17,6 +17,32 @@ export default class RickMorty {
     displayCharacters(data) {
         const characters = data.results;
         console.log(characters);
+        if (this.temperature < 0) {
+            const character = characters[0];
+            console.log(character);
+            document.querySelector('.character__name').textContent = character.name;
+            document.querySelector('.character__image').src = character.image;
+        } else if (this.temperature < 10) {
+            const character = characters[1];
+            console.log(character);
+            document.querySelector('.character__name').textContent = character.name;
+            document.querySelector('.character__image').src = character.image;
+        } else if (this.temperature < 20) {
+            const character = characters[2];
+            console.log(character);
+            document.querySelector('.character__name').textContent = character.name;
+            document.querySelector('.character__image').src = character.image;
+        } else if (this.temperature < 30) {
+            const character = characters[3];
+            console.log(character);
+            document.querySelector('.character__name').textContent = character.name;
+            document.querySelector('.character__image').src = character.image;
+        } else {
+            const character = characters[4];
+            console.log(character);
+            document.querySelector('.character__name').textContent = character.name;
+            document.querySelector('.character__image').src = character.image;
+        }
         characters.forEach(character => {
             const characterName = character.name;
     
